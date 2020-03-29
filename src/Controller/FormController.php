c2783592aa5066b3094c0bdd298e3f10dcae31f3
Marouan Chakran <Marouane.chakran@gmail.com>
2020-03-29 16:44:26 +0200

    user registration v1

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Form\RegisterType;
 use App\Entity\User;
 
@@ -12,10 +13,19 @@ class FormController extends AbstractController
     /**
      * @Route("/index", name="form")
      */
-    public function index()
+    public function index(Request $request)
     {
         $user=new User();
-        $form = $this->createForm(RegisterType::class,$user);
+        $form = $this->createForm(RegisterType::class,$user,[
+            'action'=>$this->generateUrl('form')
+        ]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
         return $this->render('indeex.html.twig', [
             'form' => $form->createView()
         ]);
