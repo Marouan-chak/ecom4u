de85647a21143133f51ad3aeeaff9b2bf2ecb14c
Marouan Chakran <Marouane.chakran@gmail.com>
2020-03-30 16:38:45 +0200

    commit

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,18 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
      */
-    public function index()
-    {
-        return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
-        ]);
-    }
     public function login(Request $request, AuthenticationUtils $utils)
     {
         $error=$utils->getLastAuthenticationError();
