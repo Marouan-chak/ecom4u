c7422e2b903510545bc348441672bb6517986a31
Marouan Chakran <Marouane.chakran@gmail.com>
2020-03-31 16:28:22 +0200

    working login: bugs in product view after login

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -30,6 +30,25 @@ class IndexController extends AbstractController
         }
         return new JsonResponse(json_encode($response));
     }
+    public function findProduct($idProduct){
+        //$productId = $request->attributes->get('_route_params');
+        //Find product by id and return a json object with all product information
+        $repository = $this->getDoctrine()->getRepository(Produit::class);
+        $product = $repository->find($idProduct);
+        
+        $response[] = array(
+        'id' => $product->getId(),
+        'name' => $product->getName(),
+        'categorie' => $product->getCategorie(),
+        'description' => $product->getDescription(),
+        'prix' => $product->getPrix(),
+        'image' => $product->getImage(),
+        // other fields
+        );
+        
+        return new Response(json_encode($response));
+        }
+        
     public function ProductsByCategorieId($idCategorie){
         $repository = $this->getDoctrine();
         $repProduit=$repository->getRepository(Produit::class);
@@ -62,22 +81,7 @@ class IndexController extends AbstractController
 
     public function Array()
     {
-        //Get all products
-        /*$repository = $this->getDoctrine()->getRepository(Produit::class);
-        $products = $repository->findAll();
-        $response = array();
-        foreach ($products as $product) {
-            $response[] = array(
-                'id' => $product->getId(),
-                'name' => $product->getName(),
-                'categorie' => $product->getCategorie(),
-                'description' => $product->getDescription(),
-                'prix' => $product->getPrix(),
-                'image' => $product->getImage(),
-                // other fields
-            );
-        }
-        return new JsonResponse(json_encode($response));}*/
+        
         $repository = $this->getDoctrine()->getRepository(Categories::class);
         $categories = $repository->findAll();
         $response = array();
@@ -90,34 +94,7 @@ class IndexController extends AbstractController
         }
         return new JsonResponse(json_encode($response));}
 
-    public function Informatique()
-    {
-        return $this->json(array (array("name"=>"Clavier Logitech","id"=>1),
-        array("name"=>"HDD 500GB","id"=>2),
-        array("name"=>"Souris Logitech","id"=>3),
-        array("name"=>"Routeur Cisco","id"=>4)
-    ));}
-    public function Multimedia()
-    {
-        return $this->json(array (array("name"=>"Casque Beats","id"=>1),
-        array("name"=>"Enceinte JBL","id"=>2),
-        array("name"=>"Ecouteurs AKG","id"=>3)
-    ));}
-    public function Telephone()
-    {
-        return $this->json(array (array("name"=>"Galaxy s10+","id"=>1,"pic1"=>"https://www.cdiscount.com/pdt2/2/8/n/1/400x400/samgalaxs10p128n/rw/samsung-galaxy-s10-128-go-noir-prisme.jpg","pic2"=>"https://www.notebookcheck.biz/fileadmin/_processed_/3/5/csm_Samsung_Galaxy_S10_Plus_5594_5bd9b9c7be.jpg","prix"=>749),
-        array("name"=>"Google Pixel 4","id"=>2,"pic1"=>"https://www.cdiscount.com/pdt2/6/6/9/1/700x700/goo0842776115669/rw/google-smartphone-pixel-4-xl-64-go-simplement-noir.jpg","pic2"=>"https://www.cdiscount.com/pdt2/4/3/0/1/700x700/goo0842776115430/rw/google-pixel-4-64go-blanc.jpg","prix"=>946),
-        array("name"=>"Huawei P30 Pro","id"=>3,"pic1"=>"https://www.cdiscount.com/pdt2/2/8/b/1/700x700/huaweip30pro128b/rw/huawei-p30-pro-noir-128-go.jpg","pic2"=>"https://www.cdiscount.com/pdt2/2/8/b/2/700x700/huaweip30pro128b/rw/huawei-p30-pro-noir-128-go.jpg","prix"=>649),
-        array("name"=>"Iphone 11 Pro","id"=>4,"pic1"=>"https://www.media-rdc.com/medias/7b35afb233403726994c0397dfc41bdc/p_580x580/apple-iphone-11-pro-gris-sideral.png","pic2"=>"https://www.media-rdc.com/medias/6ee06f2d6d803f82a3e42e71059bc713/p_580x580/apple-iphone-11-pro-gris-sideral-2.png","prix"=>1049),
-    ));}
-    public function Ordinateur()
-    {
-        return $this->json(array (array("name"=>"Razer blade","id"=>1),
-        array("name"=>"MacBook Pro","id"=>2),
-        array("name"=>"Dell Aspiron","id"=>3),
-        array("name"=>"Lenovo 134","id"=>4)
-    ));
-    }
+
     public function addUser()
     {
         
@@ -142,11 +119,6 @@ class IndexController extends AbstractController
         
         return new Response($a);
     }
-    public function findProduct($idProduct){
-        //$productId = $request->attributes->get('_route_params');
-        //Find product by id and return a json object with all product information
-        return $this->json(array ("name"=>"Galaxy s10+","desc"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ad culpa assumenda corporis iusto quisquam hic amet incidunt eos molestias, perferendis, quia ullam, minus sapiente suscipit. Debitis at quam veritatis!","id"=>1,"pic1"=>"https://www.cdiscount.com/pdt2/2/8/n/1/400x400/samgalaxs10p128n/rw/samsung-galaxy-s10-128-go-noir-prisme.jpg","pic2"=>"https://www.notebookcheck.biz/fileadmin/_processed_/3/5/csm_Samsung_Galaxy_S10_Plus_5594_5bd9b9c7be.jpg","prix"=>749)
-);
-    }
+    
 }
 
